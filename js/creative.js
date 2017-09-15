e115f8acc1afa56b163075a15472ecc2d32e1c1a
Heigo <heigo.kure@khk.ee>
2017-09-15 18:51:48 +0300

    Video changed

--- js/creative.js
+++ js/creative.js
@@ -84,7 +84,6 @@
 
 
     var vid = document.getElementById("bgvid");
-    var pauseButton = document.querySelector("#polina button");
 
     if (window.matchMedia('(prefers-reduced-motion)').matches) {
         vid.removeAttribute("autoplay");
