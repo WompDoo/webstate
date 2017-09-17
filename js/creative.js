bcc570dfbb0812636a1f7c02fd1b62f051e323fa
Heigo <heigo.kure@khk.ee>
2017-09-17 15:40:51 +0300

    Contact froms done!

--- js/creative.js
+++ js/creative.js
@@ -103,6 +103,30 @@
         vidFade();
     });
 
+    $(document).ready(function() {
+        $("#slider-range-min").slider({
+            range: "min",
+            step: 100,
+            value: 1000,
+            min: 0,
+            max: 15000,
+            slide: function(event, ui) {
+                $("#amount").val("€" + ui.value);
+            },
+            change: function(event, ui) {
+                if (ui.value === 0) {
+                    $("#amount").val("To be determined");
+                } else if (ui.value === 15000) {
+                    $("#amount").val("€" + "15000 or more");
+                } else {
+                    $("#amount").val("€" + $("#slider-range-min").slider("value"));
+                }
+            }
+        });
+        $("#amount").val("€" + $("#slider-range-min").slider("value"));
+
+    });
+
 
 
 })(jQuery); // End of use strict
